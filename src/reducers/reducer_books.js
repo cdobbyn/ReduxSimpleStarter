e6b090dba4c56224909cb7a2631d14b6363c1187
Chris Dobbyn <cdobbyn@gmail.com>
2017-04-29 15:53:41 -0700

    asdf

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dobbyn on 4/29/2017.
+ */
+
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 39},
+        {title: 'The Dark Tower', pages: 85},
+        {title: 'Eloquent Ruby', pages: 1},
+    ]
+}
