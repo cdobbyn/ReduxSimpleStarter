61b5db15800081ed2be378e5bd8604f3b9609469
cdobbyn <cdobbyn@gmail.com>
2017-05-05 16:18:52 -0700

    Lecture 146 complete.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -15,11 +16,23 @@ export function fetchPosts() {
     };
 }
 
-export function createPost(values) {
-    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+    // using the promise, run the callback to go to the post list page
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+        .then(() => callback());
 
     return {
         type: CREATE_POST,
+        payload: request
+    };
+}
+
+export function fetchPost(id) {
+    console.log(id);
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_POST,
         payload: request
     };
 }
---
+++ src/components/posts_show.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {fetchPost} from '../actions';
+
+class PostsShow extends Component {
+    componentDidMount(){
+        const id = this.props.match.params.id;
+        this.props.fetchPost(id);
+    }
+
+    render() {
+        const {post} = this.props;
+
+        if (!post){
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <Link to="/">Back To Index</Link>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({posts}, ownProps) {
+    // ownProps === this.props
+    return {post: posts[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps,{fetchPost})(PostsShow);
